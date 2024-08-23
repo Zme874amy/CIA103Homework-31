093d5d2819c28de1f130b20c6abb23791ab67f52
Wynn Liu <zme874amy@gmail.com>
2024-08-23 14:04:05 +0800

    refactor the class name

--- src/hw1/hw1.java
+++ src/hw1/hw1.java
@@ -1,6 +1,6 @@
 package hw1;
 
-public class hw1 {
+public class Hw1 {
 	public static void main(String[] args) {
 //	• 請設計一隻Java程式,計算12,6這兩個數值的和與積
 		System.out.println("積為" + 12 * 6);
--- src/hw2/hw2.java
+++ src/hw2/hw2.java
@@ -1,6 +1,6 @@
 package hw2;
 
-public class hw2 {
+public class Hw2 {
 	public static void main(String[] args) {
 //	• 請設計一隻Java程式,計算1~1000的偶數和 (2+4+6+8+...+1000)
 		int sum=0;
